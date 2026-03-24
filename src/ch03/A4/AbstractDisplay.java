2efc89811a83b28e9d09295bf9866879c40b4ca4
JH <park0504jh@gmail.com>
2026-03-24 10:00:50 +0900

    0324

--- src/ch03/A4/AbstractDisplay.java
+++ src/ch03/A4/AbstractDisplay.java
@@ -1,11 +1,11 @@
 package ch03.A4;
 
-public interface AbstractDisplay {
+public interface AbstractDisplay {  // interface로 정의
     public void open();
     public void print();
     public void close();
 
-    public default void display() {
+    public default void display() {  // default 키워드로 display() 메소드 구현
         open();
         for (int i = 0; i < 5; i++) {
             print();
