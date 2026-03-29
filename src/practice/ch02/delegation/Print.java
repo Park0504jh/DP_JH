3a565ca031c21f414405feeff4fbb1f3e1b3fc53
JH <park0504jh@gmail.com>
2026-03-30 08:44:59 +0900

    Clarify comment on Print class regarding delegation
    Updated comment to clarify the difference between interface and abstract class.

--- src/practice/ch02/delegation/Print.java
+++ src/practice/ch02/delegation/Print.java
@@ -1,6 +1,6 @@
 package practice.ch02.delegation;
 
-// 클라이언트가 원하는 인터페이스 (추상 클래스)
+// 클라이언트가 원하는 인터페이스 (추상 클래스) - 01에서는 인터페이스라 상속이 가능했지만, 02에서는 추상이라 안됨 -> 위임
 public abstract class Print {
     public abstract void printWeak();
     public abstract void printStrong();
