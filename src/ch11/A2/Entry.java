0d663bb7e0f2a18642ed3d906bae46ff167b22d8
JH <park0504jh@gmail.com>
2026-04-21 09:46:13 +0900

    0421

--- src/ch11/A2/Entry.java
+++ src/ch11/A2/Entry.java
@@ -30,13 +30,13 @@ public abstract class Entry {
 
     // 전체 경로를 가져온다 
     public String getFullName() {
-        StringBuilder fullname = new StringBuilder();
+        StringBuilder fullname = new StringBuilder();  // 전체 경로 변수 fullname
         Entry entry = this;
         do {
-            fullname.insert(0, entry.getName());
-            fullname.insert(0, "/");
-            entry = entry.parent;
-        } while (entry != null);
+            fullname.insert(0, entry.getName());  // 0번 인덱스에 현재 엔트리의 이름을 삽입
+            fullname.insert(0, "/");  // 0번 인덱스에 "/"를 삽입하여 경로 구분자 추가
+            entry = entry.parent;  // 부모로 이동 (트리의 윗 방향으로 이동)
+        } while (entry != null);  // 루트 디렉터리에 도달할 때까지 반복
         return fullname.toString();
     }
 }
--- src/practice/ch11/Main.java
+++ src/practice/ch11/Main.java
@@ -3,7 +3,17 @@ package practice.ch11;
 public class Main {
     public static void main(String[] args) {
         // 디렉터리 트리를 만든다
-        Directory root = new Directory("root");
+
+        Entry root = new Directory("root");  // 업캐스팅(자동으로 일어나므로 신경쓸 필요 없음)
+        // 부모 타입으로 선언하면 자식 사용 가능
+        // 근데 이러면 Entry에 add() 메소드가 없어서 add() 메소드를 사용할 수 없다.
+        // -> 형변환! 
+        ((Directory) root).add(bin);  // 다운캐스팅(명시적으로 해야 한다)
+        // . 이 타입 캐스팅보다 우선 순위가 높다
+        // 그러므로 괄호로 묶어줘야 한다
+
+
+        // Directory root = new Directory("root");
         Directory bin = new Directory("bin");
         Directory tmp = new Directory("tmp");
         Directory usr = new Directory("usr");
