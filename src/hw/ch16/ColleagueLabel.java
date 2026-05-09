fc8bf29e6ca3ce131004f49b1b5832b0f8f02e1b
박정현 <park0504jh@gmail.com>
2026-05-09 20:14:57 +0900

    hw16

--- src/hw/ch16/ColleagueLabel.java
+++ src/hw/ch16/ColleagueLabel.java
@@ -3,7 +3,7 @@ package hw.ch16;
 import java.awt.Color;
 import java.awt.Label;
 
- 
+
 public class ColleagueLabel extends Label implements Colleague {
     private Mediator mediator; 
 
@@ -21,12 +21,12 @@ public class ColleagueLabel extends Label implements Colleague {
     @Override
     public void setColleagueEnabled(boolean enabled) {
         if (enabled) {
-            // 로그인 가능할 때: 파란색 글씨와 " 로그인 가능" 텍스트 설정 
-            setText(" 로그인 가능");
+            // 로그인 가능할 때: 파란색 글씨와 "로그인 가능" 텍스트 설정 
+            setText("로그인 가능");
             setForeground(Color.blue);
         } else {
-            // 로그인 불가할 때: 회색 글씨와 " 로그인 불가" 텍스트 설정 
-            setText(" 로그인 불가");
+            // 로그인 불가할 때: 회색 글씨와 "로그인 불가" 텍스트 설정 
+            setText("로그인 불가");
             setForeground(Color.gray);
         }
     }
